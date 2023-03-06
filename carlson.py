41c3c5d8de0506c876d10383cd34f21d850b8604
Collin Laney <95191733+ColDog5044@users.noreply.github.com>
2023-03-06 08:35:02 -0500

    Add CARLSON interaction and requests

--- carlson.py
+++ carlson.py
@@ -1,6 +1,7 @@
-import speech_recognition as sr
-import pyttsx3
+import speech_recognition as sr #*
+import pyttsx3 #*
 import datetime
+import pyjokes #*
 
 # initialize text-to-speech engine
 engine = pyttsx3.init()
@@ -27,7 +28,7 @@ def recognize_speech():
         print(f"You said: {query}\n")
     except Exception as e:
         print(e)
-        speak('Sorry, I didn\'t catch that. Can you please repeat?')
+        speak('Sorry, I did not understand that. Can you please repeat it?')
         return "None"
     return query
 
@@ -35,13 +36,13 @@ def recognize_speech():
 def greet():
     hour = int(datetime.datetime.now().hour)
     if hour >= 0 and hour < 12:
-        speak("Good morning,")
+        speak(choice(morningResponses))
     elif hour >= 12 and hour < 18:
-        speak("Good afternoon,")
+        speak("Good afternoon sir,")
     else:
-        speak("Good evening,")
+        speak("Good evening sir,")
 
-    speak("my name is CARLSON, how may I be of service?")
+    speak('''CARLSON is at your service sir.''')
 
 # main program loop
 if __name__ == "__main__":
@@ -53,6 +54,42 @@ if __name__ == "__main__":
         if 'what time is it' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")
             speak(f"The time is {strTime}")
+            
+        ########## CARLSON INTERACTION ##########
+        
+        elif "what is your name" in query:
+            speak("I am CARLSON, Computer Automated Robotic Listening Software Operations Navigation. How can I be of service sir?")
+            continue
+
+        elif "who created you" in query:
+            speak("I was created by Collin Laney, on October Thirteenth, Twenty Twenty-Two. However, this version of me was created on March Sixth, Twenty Twenty-Three.")
+            continue
+
+        elif "what is your birthday" in query:
+            speak("March Sixth, Twenty Twenty-Three")
+            continue
+
+        elif "hello" in query:
+            speak("Hello sir, how are you?")
+            continue
+
+        elif "i am" in query or "i'm" in query:
+            speak("That's great sir.")
+            continue
+            
+        elif "how are you" in query:
+            speak("Perfect sir.")
+            continue
+            
+        elif "thank you" in query:
+            speak("You are welcome sir.")
+            continue
+
+        elif "joke" in query:
+            speak(pyjokes.get_joke())
+            continue
+            
+         ########## CARLSON REQUESTS ##########
 
         elif 'exit' in query:
             speak("Goodbye!")
